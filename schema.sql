b65c97e63a7cc5aa2529749438bf425c7f04a443
jalvsaker <jalvsaker@users.noreply.github.com>
2023-08-25 23:26:57 +0200

    save to turso db

---
+++ schema.sql
@@ -0,0 +1,7 @@
+create table comments (
+    name text,
+    comment text
+);
+
+ALTER TABLE comments
+ADD date text;
--- src/comments.ts
+++ src/comments.ts
@@ -1,3 +1,10 @@
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+  url: import.meta.env.DB_URL,
+  authToken: import.meta.env.DB_TOKEN,
+});
+
 export interface Comment {
   id?: string;
   author: string;
@@ -5,19 +12,44 @@ export interface Comment {
   date: Date;
 }
 
-let comments: Comment[] = [];
+export async function removeCommentById(id: string) {
+  const rs = await client.execute({
+    sql: "delete from comments where rowid = :id",
+    args: {
+      id,
+    },
+  });
 
-let lastId = 0;
-
-export function removeCommentById(id: string) {
-  comments = comments.filter((comment) => comment.id !== id);
+  if (rs.rowsAffected < 1) {
+    throw new Error();
+  }
 }
 
-export function addComment(comment: Comment) {
-  comment.id = String(++lastId);
-  comments.push(comment).toString();
+export async function addComment(comment: Comment) {
+  console.log(comment.date.toISOString());
+  const rs = await client.execute({
+    sql: "insert into comments values (:name, :text, :date);",
+    args: {
+      name: comment.author,
+      text: comment.text,
+      date: comment.date.toUTCString(),
+    },
+  });
+  comment.id = rs.lastInsertRowid?.toString();
 }
 
-export function getAllComments(): Comment[] {
-  return comments;
+export async function getAllComments(): Promise<Comment[]> {
+  const rs = await client.execute(
+    "select rowid, name, comment, date from comments;"
+  );
+
+  const result = rs.rows.map((row) => {
+    return {
+      id: row.rowid?.toString(),
+      author: String(row.name),
+      text: String(row.comment),
+      date: new Date(String(row.date)),
+    };
+  });
+  return result;
 }
