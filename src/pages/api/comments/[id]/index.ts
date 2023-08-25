ab53c88568371a05daaef869b0362c40e4a712c7
jalvsaker <jalvsaker@users.noreply.github.com>
2023-08-25 23:47:36 +0200

    make delete work

--- src/pages/api/comments/[id]/index.ts
+++ src/pages/api/comments/[id]/index.ts
@@ -1,11 +1,11 @@
 import type { APIRoute } from "astro";
 import { removeCommentById } from "../../../../comments";
 
-export const del: APIRoute = ({ params, request }) => {
+export const del: APIRoute = async ({ params, request }) => {
   const { id } = params;
   if (!id) {
     return new Response("", { status: 400 });
   }
-  removeCommentById(id);
+  await removeCommentById(id);
   return new Response();
 };
